b64b095ea37a1c7ba9bfd5a774f46366981d49fa
neji <neji@bamboocat.com>
2017-06-08 18:17:15 +0800

    project更換呈現方式

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,9 +1,38 @@
+function full(obj) {
+	if (obj.parents(".box").hasClass('full') ) {
+		obj.parents(".box").removeClass('full').css('top', "10%").css('left', "10%");
+	} else {
+		obj.parents(".box").addClass('full').css('top', 0).css('left', 0);
+	}
+};
+
 $(".close").click(function() {
 	$(this).parents(".box").hide(100);
 });
 
 $(".desktop a").dblclick(function() {
 	$("#" + $(this).data('target')).show(100);
-})
+});
 
 $(".desktop a, .box").draggable();
+$(".content a").draggable({
+	                containment: ".content",
+	                scroll: false
+	        	});
+
+$(".box > .tool").dblclick(function() {
+	full($(this));
+});
+
+$(".full-btn").click(function() {
+	full($(this));
+})
+
+$(".content a").dblclick(function() {
+	$("#" + $(this).data('target')).show(100);
+});
+
+$(".desktop .chrome").dblclick(function() {
+	location.href = $(this).data('target');
+});
+
